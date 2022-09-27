6fcedd7fb7e3705dcf9d0a5cca50ab9d68007e25
kp6169 <45569241+kp6169@users.noreply.github.com>
2022-09-27 22:39:49 +0100

    'AddFunction'

--- hypersheets/_modidx.py
+++ hypersheets/_modidx.py
@@ -6,5 +6,9 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/KGrewal1/hypersheets',
                 'lib_path': 'hypersheets'},
   'syms': { 'hypersheets.core': {'hypersheets.core.foo': ('core.html#foo', 'hypersheets/core.py')},
-            'hypersheets.stats': { 'hypersheets.stats.compsum': ('stats.html#compsum', 'hypersheets/stats.py'),
-                                   'hypersheets.stats.foo': ('stats.html#foo', 'hypersheets/stats.py')}}}
+            'hypersheets.stats': { 'hypersheets.stats.comp': ('stats.html#comp', 'hypersheets/stats.py'),
+                                   'hypersheets.stats.compsum': ('stats.html#compsum', 'hypersheets/stats.py'),
+                                   'hypersheets.stats.expected_return': ('stats.html#expected_return', 'hypersheets/stats.py'),
+                                   'hypersheets.stats.foo': ('stats.html#foo', 'hypersheets/stats.py'),
+                                   'hypersheets.stats.geometric_mean': ('stats.html#geometric_mean', 'hypersheets/stats.py'),
+                                   'hypersheets.stats.ghpr': ('stats.html#ghpr', 'hypersheets/stats.py')}}}
