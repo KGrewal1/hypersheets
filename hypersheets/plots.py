57791802e3d818ef1c179d5b2ad5b595f220c68c
kp6169 <45569241+kp6169@users.noreply.github.com>
2022-09-29 23:41:57 +0100

    Add reports

---
+++ hypersheets/plots.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# hypersheets: Accesible Portfolio analytics
+# https://github.com/KGrewal1/hypersheets
+#
+# Copyright 2022 Kirpal Grewal
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ORIGINAL QUANTSTATS LICENCE
+#
+# QuantStats: Portfolio analytics for quants
+# https://github.com/ranaroussi/quantstats
+#
+# Copyright 2019 Ran Aroussi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+try:
+    from pandas.plotting import register_matplotlib_converters as rmc
+    rmc()
+except ImportError:
+    pass
+
+from hypersheets._plotting.wrappers import *
